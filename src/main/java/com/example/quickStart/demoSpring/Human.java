4b9f36d044ec4caf3403c30d7d3458d6faf5b95d
Lau <lauro.bernardojr@dxc.com>
2021-07-02 14:48:39 +0800

    initial commit

---
+++ src/main/java/com/example/quickStart/demoSpring/Human.java
@@ -0,0 +1,34 @@
+package com.example.quickStart.demoSpring;
+
+public class Human {
+
+	private int id;
+	private String name;
+	private long points;
+	
+	public Human(int id, String name, long points) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.points = points;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPoints() {
+		return points;
+	}
+	public void setPoints(long points) {
+		this.points = points;
+	}
+}
