ebabb138182b70bd16da5d6e4c7483f7fd54e47c
Lau <lauro.bernardojr@dxc.com>
2021-07-02 13:13:53 +0800

    Initial commit

---
+++ src/main/java/com/example/quickStart/demoSpring/controller/GreetingsController.java
@@ -0,0 +1,30 @@
+package com.example.quickStart.demoSpring.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.quickStart.demoSpring.Greetings;
+
+
+//This is how someone would interact with our API this is their main entry point
+@RestController
+public class GreetingsController {
+	
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	// /greeting?name=Dan -> Hello Dan!
+	@GetMapping("/greeting")
+	public Greetings greeting(@RequestParam(value = "name", 
+											defaultValue = "World") String name) {
+		return new Greetings(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@GetMapping("/greetings")
+	public Greetings greetAll() {
+		return ;
+	}
+}
