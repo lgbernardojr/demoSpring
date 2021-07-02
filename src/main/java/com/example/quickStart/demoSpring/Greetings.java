6a1c3ea01202e02525e0a6393b656fc13cd777d0
Lau <lauro.bernardojr@dxc.com>
2021-07-02 14:20:33 +0800

    Update Greetings.java
    Added String name and Update the constructor and getters

--- src/main/java/com/example/quickStart/demoSpring/Greetings.java
+++ src/main/java/com/example/quickStart/demoSpring/Greetings.java
@@ -4,21 +4,25 @@ package com.example.quickStart.demoSpring;
 public class Greetings {
 	private final long id;
 	private final String content;
+	private final String name;
 	
-	public Greetings(long id, String content) {
+	public Greetings(long id, String content, String name) {
 		super();
 		this.id = id;
 		this.content = content;
+		this.name = name;
 	}
 
 	public long getId() {
 		return id;
 	}
 
+	public String getName() {
+		return name;
+	}
+
 	public String getContent() {
 		return content;
 	}
-	
-	
-	
+
 }
